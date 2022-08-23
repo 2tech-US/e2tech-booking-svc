d08079695cd81f04a028cc11ce40ef86773314be
ZabitTank <95601488+ZabitTank@users.noreply.github.com>
2022-08-24 02:59:50 +0700

    fix: migrateup

--- migration/000003_init_schema.up.sql
+++ migration/000003_init_schema.up.sql
@@ -1,12 +1,12 @@
 INSERT INTO history
  (type, passenger_phone, driver_phone, pick_up_latitude, pick_up_longitude, drop_off_latitude, drop_off_longitude, price, created_at, done_at) 
- VALUES ("app", "1111111111", "2222222222", 11, 12, 13, 14, 200, '2022/11/11', '2022/11/11');
+ VALUES ('app', '1111111111', '2222222222', 11, 12, 13, 14, 200, '2022/11/11', '2022/11/11');
  INSERT INTO history
  (type, passenger_phone, driver_phone, pick_up_latitude, pick_up_longitude, drop_off_latitude, drop_off_longitude, price, created_at, done_at) 
- VALUES ("app", "1111111112", "2222222223", 11, 12, 13, 14, 200, '2022/11/11', '2022/11/11');
+ VALUES ('app', '1111111112', '2222222223', 11, 12, 13, 14, 200, '2022/11/11', '2022/11/11');
  INSERT INTO history
  (type, passenger_phone, driver_phone, pick_up_latitude, pick_up_longitude, drop_off_latitude, drop_off_longitude, price, created_at, done_at) 
- VALUES ("app", "1111111113", "2222222224", 11, 12, 13, 14, 200, '2022/11/11', '2022/11/11');
+ VALUES ('app', '1111111113', '2222222224', 11, 12, 13, 14, 200, '2022/11/11', '2022/11/11');
  INSERT INTO history
  (type, passenger_phone, driver_phone, pick_up_latitude, pick_up_longitude, drop_off_latitude, drop_off_longitude, price, created_at, done_at) 
- VALUES ("app", "1111111114", "2222222225", 11, 12, 13, 14, 200, '2022/11/11', '2022/11/11');
+ VALUES ('app', '1111111114', '2222222225', 11, 12, 13, 14, 200, '2022/11/11', '2022/11/11');
